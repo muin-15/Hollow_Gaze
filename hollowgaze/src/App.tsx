cee746e9e8de7610b9b78899b1f25d9e5282f16e
muin-15 <140100907+muin-15@users.noreply.github.com>
2025-12-09 04:27:50 +0530

    starting page

--- hollowgaze/src/App.tsx
+++ hollowgaze/src/App.tsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleUnmute = () => {
+    const audio = document.getElementById('bg-music') as HTMLAudioElement
+    if (audio) {
+      audio.muted = false
+      audio.play()
+    }
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="intro">
+        <h1>A memory is required for entry.</h1>
+        <h1>Which will you forget?</h1>
+        <button className='startbutton' onClick={handleUnmute}>Start</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
