051bbfcdb6dfeef71243d481d077e19cd96e81e5
muin-15 <140100907+muin-15@users.noreply.github.com>
2025-12-12 01:03:35 +0530

    All changes are made for game initial page

--- hollowgaze/src/game.tsx
+++ hollowgaze/src/game.tsx
@@ -9,8 +9,12 @@ function Game(){
             </div>
                <img className='user-image' src='user_image.gif' alt='Jozu'></img>
                <p className='user-info'>Jozu</p>
+
             <div className="Start-info">
-                
+                <div className='para-start'>
+                    <p>You need to leave some of your memory points at each level to continue for next level</p>
+                    <button className='start-button'>Start Game</button>
+                </div>
             </div>
         </div>
     );
