38dde7164ed7701555d02bed6a234ffb73211f57
muin-15 <140100907+muin-15@users.noreply.github.com>
2025-12-12 20:01:15 +0530

    adding ghost image

--- hollowgaze/src/maingame.tsx
+++ hollowgaze/src/maingame.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+/*import React from 'react';
+import { useNavigate } from 'react-router-dom';*/
 import './maingame.css';
 
 function MGame() {
     return (
-        <div className="game-screen">
+        <div>
+        <div className="gaming-screen">
             <div className='upper-bg'></div>
-            <div className='lower-bg'></div>
+            <div className='lower-bg'>
+                
+            </div>
+        </div>
+        <div className='lower-front-bg'>
+            
+        </div>
+        <div className='Character'></div>
+        <div className='Whiteghost1'></div>
+        <div className='Whiteghost2'></div>
+        <div className='Whiteghost3'></div>
         </div>
     );
 }
